a670af81dff1321d094029b884062bd7cca0404f
Caminoasi <82422136+Caminoasi@users.noreply.github.com>
2022-06-26 14:01:04 -0300

    1st-Commit

--- src/main/java/com/example/exercicios/controllers/PrimeiroController.java
+++ src/main/java/com/example/exercicios/controllers/PrimeiroController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Primeiro {
+public class PrimeiroController {
 
 	@GetMapping(path = {"/ola", "/saudacao"})
 	public String ola() {
